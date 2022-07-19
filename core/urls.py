1a76e3b9bc0f1172b738fb6df6e7c58e8c4fef63
Carine Neris <carine_neris@hotmail.com>
2022-07-19 14:42:37 -0300

    adicionando rota para view produtos e criando uma view de teste

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, contato
+from .views import index, contato,produto, teste
 
 
 urlpatterns = [
     path('', index),
     path('contatos', contato),
+    path('produto/<int:pk>', produto, name='produto'),
+    path('teste',teste),
 ]
--- core/views.py
+++ core/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render
+from .models import Cliente, Produtos
 
 
 def index(request):
-    print(request.headers)
-    print(request.user)
+    itens = Produtos.objects.all()
     context = {
         'curso':'Programação web',
         'linguagem': 'Python',
-        'framework': 'Django'
+        'produtos': itens
     }
     return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request, 'contato.html')
+
+
+def teste(request):
+    print(request.headers)
+    print(request.user)
+    context = {
+        'curso': 'Programação web',
+        'linguagem': 'Python',
+        'framework': 'Django'
+    }
+    return render(request, 'teste.html', context)
+
+
+def produto(request, pk):
+    item = Produtos.objects.get(id=pk)
+    # O retorno de informações tem que sem no formato dict
+    to_dict = {
+        'nome': item.nome,
+        'preco': item.preco,
+        'quantidade': item.estoque
+    }
+    return render(request, 'produto.html',to_dict)
