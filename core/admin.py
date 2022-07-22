9cece22682f98c94b63a2ced6803a416773e4235
Carine Neris <carine_neris@hotmail.com>
2022-07-21 21:12:38 -0300

    testando busca personalizada no admin

--- core/admin.py
+++ core/admin.py
@@ -8,7 +8,25 @@ class ClienteAdmin(admin.ModelAdmin):
 
 class ProdutosAdmin(admin.ModelAdmin):
     list_display = ('nome', 'preco', 'estoque')
+    search_fields = ('nome',)
 
+    #implementação para buscar estoque
+    #ModelAdmin.get_search_results( request , queryset , search_term )
+    def get_search_results(self, request, queryset, search_term):
+        print(request)
+        print(queryset.values())
+        print(search_term)
+        queryset, may_have_duplicates = super().get_search_results(
+            request, queryset, search_term,
+        )
+        try:
+            search_term_as_int = int(search_term)
+        except ValueError:
+            pass
+        else:
+            queryset |= self.model.objects.filter(estoque=search_term_as_int)
+        print(queryset.values())
+        return queryset, may_have_duplicates
 
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Produtos, ProdutosAdmin)
